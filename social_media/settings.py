966b0dd4241392fb342c04b0f8dbefacbb4235df
Himanshu Sagar <sagarhimanshu355@gmail.com>
2022-11-21 13:40:09 +0530

    secrets from env file

--- social_media/settings.py
+++ social_media/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+x0&5^(meq4=#-k(@8y$a*i3%4h7r#+@wn9zk^86k$maa5z8d!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'social_media_db',
-        'USER': "postgres",
-        'PASSWORD': "h841207s",
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
