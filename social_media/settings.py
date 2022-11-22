4e75e6fb755cc15267d38840efc9c806b825f634
Himanshu Sagar <sagarhimanshu355@gmail.com>
2022-11-22 16:54:57 +0530

    modify

--- social_media/settings.py
+++ social_media/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["social-media-apis-6bs9.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
